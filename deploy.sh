25760b4644a79fd7b046a702c84f3c5e101d710a
Robert Bartl <robert@edv-bartl.at>
2017-02-12 11:54:33 +0100

    renamed prj

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 gradle bootRepackage
-scp ./build/libs/alexaspring-1.0-SNAPSHOT.jar root@iot.localdomain:/opt/
+scp ./build/libs/alexaremote-1.0-SNAPSHOT.jar root@iot.localdomain:/opt/
 ssh root@iot.localdomain killall java
 
